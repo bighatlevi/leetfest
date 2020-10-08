3b493e7a24636ad51eadc7b49ce245cfe27c6ee6
Levi M. Johnson <36481732+levimjohn@users.noreply.github.com>
2020-10-07 22:51:20 -0500

    Update climbingStairs.py

--- python/climbingStairs.py
+++ python/climbingStairs.py
@@ -2,7 +2,7 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # Fibonacci sequence
-        a, b = 1
+        a, b = 1, 1
         for i in range(n):
             c = b
             b = a + b
